66acbf3a4fe80fabefb3f2e75ad5ddf59c6d7089
Dhruv Shah <dhruvshah38@gmail.com>
2024-05-01 23:34:48 -0400

    added .env file

--- genomeEncoder.py
+++ genomeEncoder.py
@@ -2,13 +2,17 @@ from Bio import SeqIO
 import numpy as np
 from genomeFiles import get_file_names
 from extractSpeciesName import extractName
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def one_hot_encode(seq):
     mapping = {'A': [1, 0, 0, 0], 'C': [0, 1, 0, 0], 'G': [0, 0, 1, 0], 'T': [0, 0, 0, 1], 'N': [0, 0, 0, 0]}
     return np.array([mapping[s] for s in seq])
 
 # Load sequences
-file_directory = '/Users/dhruvshah/Projects/Gene-Sequencing-Model/Gene-Sequence-Classification/Genome Sequences/'
+file_directory = os.getenv('FILE_DIRECTORY')
 file_paths = get_file_names(file_directory)
 
 encodings = []
