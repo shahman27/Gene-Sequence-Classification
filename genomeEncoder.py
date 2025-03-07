7f4b3f0bb55cbfa36dcaf2ebad6585ca992bf206
Dhruv Shah <dhruvshah38@gmail.com>
2025-03-07 00:45:37 -0500

    eencoding all files

--- genomeEncoder.py
+++ genomeEncoder.py
@@ -16,31 +16,35 @@ def one_hot_encode(seq):
 file_directory = os.getenv('FILE_DIRECTORY')
 file_paths = get_file_names(file_directory)
 
-encodings = []
-
-# for file in file_paths:
-#     genome_dict = {}
-#     with open(file, 'r') as file:
-#         for sequence in SeqIO.parse(file, 'fasta'):        
-#             # Example: Process and encode the first sequence
-#             encoded_seq = one_hot_encode(sequence)
-#             encodings.append(encoded_seq)
 
 genome_dict = {}
-species = extractName(file_paths[1])
-current_file = file_paths[1]
-with open(current_file, 'r') as file:
-    for sequence in SeqIO.parse(file, 'fasta'):
-        encoded_seq = one_hot_encode(sequence)
-        encodings.append(encoded_seq)
+for file in file_paths:
+    with open(file, 'r') as file_handle:
+        encodings = []
+        species = extractName(file)
+        for sequence in SeqIO.parse(file_handle, 'fasta'):        
+            # Example: Process and encode the first sequence
+            encoded_seq = one_hot_encode(sequence)
+            encodings.append(encoded_seq)
+        curr_data = {species: encodings}
+        genome_dict.update(curr_data)
+    
+print(genome_dict.keys())
+# genome_dict = {}
+# species = extractName(file_paths[1])
+# current_file = file_paths[1]
+# with open(current_file, 'r') as file:
+#     for sequence in SeqIO.parse(file, 'fasta'):
+#         encoded_seq = one_hot_encode(sequence)
+#         encodings.append(encoded_seq)
 
-new_data = {species: encodings[0]}
-genome_dict.update(new_data)
+# new_data = {species: encodings[0]}
+# genome_dict.update(new_data)
 
-test_data, train_data = splitData(encodings)
+# test_data, train_data = splitData(encodings)
 
-test_train_data = {'test': test_data, 'train': train_data}
+# test_train_data = {'test': test_data, 'train': train_data}
 
-print(genome_dict) 
+# print(genome_dict) 
     
 # print(encodings)
